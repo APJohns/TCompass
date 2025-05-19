6580e42d916a7519d97b24be656023458a557a38
Ash Johns <mail@ashpjohns.com>
2025-05-19 15:03:38 -0400

    move compass into its own component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Image } from 'expo-image';
-import { FlatList, StyleSheet, useColorScheme, View } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 
+import Compass from '@/components/Compass';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -9,7 +10,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Location from 'expo-location';
 import { useCallback, useEffect, useState } from 'react';
 
-interface Station {
+export interface Station {
   id: string;
   type: string;
   attributes: {
@@ -27,8 +28,6 @@ export default function HomeScreen() {
 
   const [stations, setStations] = useState<Station[]>([]);
 
-  const colorScheme = useColorScheme() ?? 'light';
-
   const getDistanceBetweenCoordinates = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
     const R = 6371; // Radius of the Earth in km
     const dLat = ((lat2 - lat1) * Math.PI) / 180;
@@ -43,15 +42,6 @@ export default function HomeScreen() {
   const getDistanceBetweenCoordinatesInMiles = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
     const distanceInKm = getDistanceBetweenCoordinates(lat1, lon1, lat2, lon2);
     return distanceInKm * 0.621371; // Convert km to miles
-  };
-
-  const getHeadingBetweenCoordinates = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
-    const dLon = lon2 - lon1;
-    const y = Math.sin(dLon) * Math.cos(lat2);
-    const x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
-    const brng = Math.atan2(y, x);
-    const brngInDegrees = (brng * 180) / Math.PI; // Convert to degrees
-    return (brngInDegrees + 360) % 360; // Normalize to 0-360
   };
 
   const getLocation = async () => {
@@ -122,8 +112,6 @@ export default function HomeScreen() {
     getLocation();
   }, []);
 
-  const compassBorderColor = { light: '#A1CEDC', dark: '#1D3D47' };
-
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -134,13 +122,9 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Your location</ThemedText>
-        <ThemedText>{location?.coords.latitude}</ThemedText>
-        <ThemedText>{location?.coords.longitude}</ThemedText>
-        <ThemedText>{heading}</ThemedText>
-        <ThemedText type="subtitle">Closest stations</ThemedText>
-        {location && stations?.length > 0 && (
+        {location && heading && stations?.length > 0 && (
           <>
+            <Compass heading={heading} location={location} stations={stations} />
             <FlatList
               data={stations}
               renderItem={({ item }) => (
@@ -158,66 +142,6 @@ export default function HomeScreen() {
                 </>
               )}
             />
-            <View
-              style={{
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                width: '100%',
-                aspectRatio: '1/1',
-                borderWidth: 2,
-                borderColor: compassBorderColor[colorScheme],
-                borderRadius: '50%',
-                paddingHorizontal: 16,
-                paddingVertical: 8,
-                transform: [{ rotate: `-${heading}deg` }],
-              }}
-            >
-              <ThemedText>N</ThemedText>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                  alignSelf: 'stretch',
-                }}
-              >
-                <ThemedText>W</ThemedText>
-                <ThemedText>E</ThemedText>
-              </View>
-              <ThemedText>S</ThemedText>
-              {stations.map((station, index) => (
-                <View
-                  key={station.id}
-                  style={{
-                    position: 'absolute',
-                    inset: 64 - index * 16,
-                    alignItems: 'center',
-                    transform: [
-                      {
-                        rotate: `-${getHeadingBetweenCoordinates(
-                          location?.coords.latitude,
-                          location?.coords.longitude,
-                          station?.attributes.latitude,
-                          station?.attributes.longitude
-                        )}deg`,
-                      },
-                    ],
-                  }}
-                >
-                  <View
-                    style={{
-                      padding: 4,
-                      borderRadius: '50%',
-                      backgroundColor: 'orange',
-                      aspectRatio: '1/1',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <ThemedText>{index + 1}</ThemedText>
-                  </View>
-                </View>
-              ))}
-            </View>
           </>
         )}
       </ThemedView>
---
+++ components/Compass.tsx
@@ -0,0 +1,87 @@
+import { Station } from '@/app/(tabs)';
+import { LocationObject } from 'expo-location';
+import { useColorScheme, View } from 'react-native';
+import { ThemedText } from './ThemedText';
+
+interface Props {
+  heading: number;
+  location: LocationObject;
+  stations: Station[];
+}
+
+export default function Compass({ heading, location, stations }: Props) {
+  const colorScheme = useColorScheme() ?? 'light';
+  const compassBorderColor = { light: '#A1CEDC', dark: '#1D3D47' };
+
+  const getHeadingBetweenCoordinates = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
+    const dLon = lon2 - lon1;
+    const y = Math.sin(dLon) * Math.cos(lat2);
+    const x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
+    const brng = Math.atan2(y, x);
+    const brngInDegrees = (brng * 180) / Math.PI; // Convert to degrees
+    return (brngInDegrees + 360) % 360; // Normalize to 0-360
+  };
+
+  return (
+    <View
+      style={{
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        aspectRatio: '1/1',
+        borderWidth: 2,
+        borderColor: compassBorderColor[colorScheme],
+        borderRadius: '50%',
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        transform: [{ rotate: `-${heading}deg` }],
+      }}
+    >
+      <ThemedText>N</ThemedText>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignSelf: 'stretch',
+        }}
+      >
+        <ThemedText>W</ThemedText>
+        <ThemedText>E</ThemedText>
+      </View>
+      <ThemedText>S</ThemedText>
+      {stations.map((station, index) => (
+        <View
+          key={station.id}
+          style={{
+            position: 'absolute',
+            inset: 64 - index * 16,
+            alignItems: 'center',
+            transform: [
+              {
+                rotate: `-${getHeadingBetweenCoordinates(
+                  location?.coords.latitude,
+                  location?.coords.longitude,
+                  station?.attributes.latitude,
+                  station?.attributes.longitude
+                )}deg`,
+              },
+            ],
+          }}
+        >
+          <View
+            style={{
+              padding: 4,
+              borderRadius: '50%',
+              backgroundColor: 'orange',
+              aspectRatio: '1/1',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <ThemedText>{index + 1}</ThemedText>
+          </View>
+        </View>
+      ))}
+    </View>
+  );
+}
