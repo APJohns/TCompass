3f11754784f0993b738577c31aa3cff481305a58
Ash Johns <mail@ashpjohns.com>
2025-05-19 23:44:46 -0400

    fix util functions

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,12 +1,26 @@
-export const getDistanceBetweenCoordinates = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
-  const R = 6371; // Radius of the Earth in km
-  const dLat = ((lat2 - lat1) * Math.PI) / 180;
-  const dLon = ((lon2 - lon1) * Math.PI) / 180;
+// Converts from degrees to radians.
+function toRadians(degrees: number) {
+  return (degrees * Math.PI) / 180;
+}
+
+// Converts from radians to degrees.
+function toDegrees(radians: number) {
+  return (radians * 180) / Math.PI;
+}
+
+export const getDistanceBetweenCoordinates = (latA: number, lonA: number, latB: number, lonB: number): number => {
+  const R = 6371e3; // metres
+  const lat1 = toRadians(latA);
+  const lat2 = toRadians(latB);
+  const dlat = toRadians(latB - latA);
+  const dlon = toRadians(lonB - lonA);
+
   const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos((lat1 * Math.PI) / 180) * Math.cos((lat2 * Math.PI) / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    Math.sin(dlat / 2) * Math.sin(dlat / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) * Math.sin(dlon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return R * c; // Distance in km
+
+  const d = R * c; // in metres
+  return d / 1000; // in km
 };
 
 export const getDistanceBetweenCoordinatesInMiles = (
@@ -19,11 +33,13 @@ export const getDistanceBetweenCoordinatesInMiles = (
   return distanceInKm * 0.621371; // Convert km to miles
 };
 
-export const getHeadingBetweenCoordinates = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
-  const dLon = lon2 - lon1;
-  const y = Math.sin(dLon) * Math.cos(lat2);
-  const x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
-  const brng = Math.atan2(y, x);
-  const brngInDegrees = (brng * 180) / Math.PI; // Convert to degrees
-  return (brngInDegrees + 360) % 360; // Normalize to 0-360
+export const getHeadingBetweenCoordinates = (latA: number, lonA: number, latB: number, lonB: number): number => {
+  const lat1 = toRadians(latA);
+  const lon1 = toRadians(lonA);
+  const lat2 = toRadians(latB);
+  const lon2 = toRadians(lonB);
+  const y = Math.sin(lon2 - lon1) * Math.cos(lat2);
+  const x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1);
+  const heading = Math.atan2(y, x);
+  return toDegrees(heading);
 };
